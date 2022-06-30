2eaa11f5ee13becafa03716b13d3c38c60da1af0
Sergey Borovikov <serg.there@gmail.com>
2022-06-30 19:25:02 +0300

    specify dep entry

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import { defineConfig } from "vite"
 export default defineConfig({
   server: {
     open: true,
+    host: true
+  },
+  optimizeDeps: {
+    entries: "./src/index.ts"
   },
   build: {
     target: "esnext",
