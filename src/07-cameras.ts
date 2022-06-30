262e42d7cf2ff821671000ad3ff40ef97fb1c1ae
Sergey Borovikov <serg.there@gmail.com>
2022-06-30 19:12:01 +0300

    restructured

--- src/07-cameras.ts
+++ src/07-cameras.ts
@@ -1,6 +1,4 @@
-const {
-  Scene, Group, BoxGeometry, MeshBasicMaterial, Mesh, Color, PerspectiveCamera, WebGLRenderer, AxesHelper
-} = THREE
+import * as THREE from "three"
 
 // Sizes
 const sizes = {
@@ -9,16 +7,16 @@ const sizes = {
 }
 
 // Scene
-const scene = new Scene()
+const scene = new THREE.Scene()
 
 // Red cube
-const group = new Group()
+const group = new THREE.Group()
 for (let x = 0; x < 3; x++) {
   for (let z = 0; z < 3; z++) {
     for (let y = 0; y < 3; y++) {
-      const cube = new Mesh(
-        new BoxGeometry(0.5, 0.5, 0.5),
-        new MeshBasicMaterial({ color: new Color(x/2, z/2, y/2) })
+      const cube = new THREE.Mesh(
+        new THREE.BoxGeometry(0.5, 0.5, 0.5),
+        new THREE.MeshBasicMaterial({ color: new THREE.Color(x/2, z/2, y/2) })
       )
       cube.position.set(x - 1, z - 1, y - 1)
       group.add(cube)
@@ -29,27 +27,26 @@ group.scale.set(0.5, 0.5, 0.5)
 scene.add(group)
 
 // Axes
-const axes = new AxesHelper(100)
+const axes = new THREE.AxesHelper(100)
 scene.add(axes)
 
 // Camera
-const camera = new PerspectiveCamera(75, sizes.width / sizes.height)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.set(1,1,2)
 camera.lookAt(axes.position)
 scene.add(camera)
 
 // Renderer
-const canvas = document.querySelector("canvas")
-const renderer = new WebGLRenderer({ canvas })
+const canvas = document.querySelector("canvas")!
+const renderer = new THREE.WebGLRenderer({ canvas })
 renderer.setSize(sizes.width, sizes.height)
 
 
 
-const loop = () => {
+export const loop = () => {
   group.rotateX(0.01)
   group.rotateY(0.0135)
   group.rotateZ(-0.0051)
   renderer.render(scene, camera)
   requestAnimationFrame(loop)
 }
-requestAnimationFrame(loop)
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,4 @@
 import "./style.sass"
+import { loop } from "./07-cameras"
 
-const PI = Math.PI, TAU = PI * 2
-
-const canvas = document.querySelector("canvas")!
-const { width, height } = canvas.getBoundingClientRect()
-canvas.width = width
-canvas.height = height
-
-const ctx = canvas.getContext("2d")!
-
-ctx.font = "32px serif"
-ctx.textAlign = "center"
-ctx.textBaseline = "middle"
-ctx.fillText("Jora, privet!", width / 2, height / 2)
-
-ctx.arc(width / 2, height / 2, 100, 0, TAU)
-ctx.stroke()
+loop()
