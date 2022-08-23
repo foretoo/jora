d430fe6e2d3b9d7f9fb29db7c85fbfced3193239
Sergey Borovikov <serg.there@gmail.com>
2022-08-23 18:37:20 +0300

    add lorenz, threeScroll, deJong attractors and rndPoints helper

--- src/misc/repl/attractors.ts
+++ src/misc/repl/attractors.ts
@@ -1,11 +1,23 @@
 let t0: number
 
+const rndPoints = (
+  points: Float32Array,
+  i: number,
+  roughness: number,
+) => {
+  const k = Math.sqrt(roughness)
+  points[i * 3 + 0] *= 1 + Math.random() * k
+  points[i * 3 + 1] *= 1 + Math.random() * k
+  points[i * 3 + 2] *= 1 + Math.random() * k
+}
+
 
 
 let thomasB = 0.208186
 export const thomasAttractorTick = (
   points: Float32Array,
-  t: number
+  t: number,
+  roughness: number,
 ) => {
   const dt = (t - t0 || 0) / 618
   const max = points.length / 3
@@ -19,12 +31,7 @@ export const thomasAttractorTick = (
     points[i * 3 + 1] = y + dt * (Math.sin(z) - thomasB * y)
     points[i * 3 + 2] = z + dt * (Math.sin(x) - thomasB * z)
 
-    if (Math.random() > 0.99) {
-      points[i * 3] *= 1.1
-      points[i * 3 + 1] *= 1.1
-      points[i * 3 + 2] *= 1.1
-    }
-
+    if (Math.random() > 1 - roughness) rndPoints(points, i, roughness)
   }
   t0 = t
 }
@@ -32,12 +39,12 @@ export const thomasAttractorTick = (
 
 
 let
-  aizawaA = 0.55,
-  aizawaB = 0.7,
-  aizawaC = 0.6,
-  aizawaD = 3.5,
-  aizawaE = 0.25,
-  aizawaF = 0.1
+  aizawaA = 0.95, // height of torus
+  aizawaB = 0.5, // attract to polars >1 top, <0 bottom
+  aizawaC = 1.40, // attratct to torus
+  aizawaD = 0.01, // rotating by Y
+  aizawaE = 0.15, // more tiny torus
+  aizawaF = 0.01  // skew -> mobius ribbon
 
   // aizawaA = 0.1, // 0.95
   // aizawaB = 0.7,
@@ -48,9 +55,10 @@ let
 
 export const aizawaAttractorTick = (
   points: Float32Array,
-  t: number
+  t: number,
+  roughness: number,
 ) => {
-  const dt = (t - t0 || 0) / 3000
+  const dt = (t - t0 || 0) / 2000
   const max = points.length / 3
 
   for (let i = 0; i < max; i++) {
@@ -62,12 +70,7 @@ export const aizawaAttractorTick = (
     points[i * 3 + 1] = y + dt * (aizawaD * x + (z - aizawaB) * y)
     points[i * 3 + 2] = z + dt * (aizawaC + aizawaA * z - (z * z * z) / 3 - (x * x + y * y) * (1 + aizawaE * z) + aizawaF * z * x * x * x)
 
-    if (Math.random() > 0.99) {
-      points[i * 3] *= 1.1
-      points[i * 3 + 1] *= 1.1
-      points[i * 3 + 2] *= 1.1
-    }
-
+    if (Math.random() > 1 - roughness) rndPoints(points, i, roughness)
   }
   t0 = t
 }
@@ -80,7 +83,8 @@ let
 
 export const sprottAttractorTick = (
   points: Float32Array,
-  t: number
+  t: number,
+  roughness: number,
 ) => {
   const dt = (t - t0 || 0) / 3000
   const max = points.length / 3
@@ -94,12 +98,7 @@ export const sprottAttractorTick = (
     points[i * 3 + 1] = y + dt * (1  -  sprottB * x * x  +  y * z)
     points[i * 3 + 2] = z + dt * (x  -  x * x  -  y * y)
 
-    if (Math.random() > 0.99) {
-      points[i * 3] *= 1.1
-      points[i * 3 + 1] *= 1.1
-      points[i * 3 + 2] *= 1.1
-    }
-
+    if (Math.random() > 1 - roughness) rndPoints(points, i, roughness)
   }
   t0 = t
 }
@@ -111,7 +110,8 @@ let
 
 export const halvorsenAttractorTick = (
   points: Float32Array,
-  t: number
+  t: number,
+  roughness: number,
 ) => {
   const dt = (t - t0 || 0) / 5000
   const max = points.length / 3
@@ -125,12 +125,99 @@ export const halvorsenAttractorTick = (
     points[i * 3 + 1] = y + dt * (-halvorsenA * y - 4 * z - 4 * x - z * z)
     points[i * 3 + 2] = z + dt * (-halvorsenA * z - 4 * x - 4 * y - x * x)
 
-    if (Math.random() > 0.99) {
-      points[i * 3] *= 1.1
-      points[i * 3 + 1] *= 1.1
-      points[i * 3 + 2] *= 1.1
-    }
+    if (Math.random() > 1 - roughness) rndPoints(points, i, roughness)
+  }
+  t0 = t
+}
 
+
+
+let
+  lorenzA = 10,
+  lorenzB = 28,
+  lorenzC = 8/3
+
+export const lorenzAttractorTick = (
+  points: Float32Array,
+  t: number,
+  roughness: number,
+) => {
+  const dt = (t - t0 || 0) / 10000
+  const max = points.length / 3
+
+  for (let i = 0; i < max; i++) {
+    const x = points[i * 3 + 0]
+    const y = points[i * 3 + 1]
+    const z = points[i * 3 + 2]
+
+    points[i * 3 + 0] = x + dt * (lorenzA * (-x + y))
+    points[i * 3 + 1] = y + dt * (-x * z + lorenzB * x - y)
+    points[i * 3 + 2] = z + dt * (x * y - lorenzC * z)
+
+    if (Math.random() > 1 - roughness) rndPoints(points, i, roughness)
+  }
+  t0 = t
+}
+
+
+
+let
+  tsA = 32.48,
+  tsB = 45.84,
+  tsC = 1.18,
+  tsD = 0.13,
+  tsE = 0.57,
+  tsF = 14.7
+
+
+export const threeScrollAttractorTick = (
+  points: Float32Array,
+  t: number,
+  roughness: number,
+) => {
+  const dt = (t - t0 || 0) / 50000
+  const max = points.length / 3
+
+  for (let i = 0; i < max; i++) {
+    const x = points[i * 3 + 0]
+    const y = points[i * 3 + 1]
+    const z = points[i * 3 + 2]
+
+    points[i * 3 + 0] = x + dt * (tsA * (y - x)  +  tsD * x * z)
+    points[i * 3 + 1] = y + dt * (tsB * x  -  x * z  +  tsF * y)
+    points[i * 3 + 2] = z + dt * (tsC * z  +  x * y  -  tsE * x * x)
+
+    if (Math.random() > 1 - roughness) rndPoints(points, i, roughness)
+  }
+  t0 = t
+}
+
+
+
+let
+  djA =  0.97,
+  djB = -1.91,
+  djC =  1.41,
+  djD = -1.51
+
+export const deJongAttractorTick = (
+  points: Float32Array,
+  t: number,
+  roughness: number,
+) => {
+  const dt = (t - t0 || 0) / 1000
+  const max = points.length / 3
+
+  for (let i = 0; i < max; i++) {
+    const x = points[i * 3 + 0]
+    const y = points[i * 3 + 1]
+    const z = points[i * 3 + 2]
+
+    points[i * 3 + 0] = Math.sin(djA * y) - Math.cos(djB * x)
+    points[i * 3 + 1] = Math.sin(djC * x) - Math.cos(djD * y)
+    points[i * 3 + 2] = z
+
+    if (Math.random() > 1 - roughness) rndPoints(points, i, roughness)
   }
   t0 = t
 }
--- src/misc/repl/index.ts
+++ src/misc/repl/index.ts
@@ -1,6 +1,6 @@
 import { createPSProgram } from "./createPSProgram"
 import { OrbitControls } from "./orbitControls"
-import { aizawaAttractorTick, halvorsenAttractorTick, sprottAttractorTick, thomasAttractorTick } from "./attractors"
+import { aizawaAttractorTick, deJongAttractorTick, halvorsenAttractorTick, lorenzAttractorTick, thomasAttractorTick, threeScrollAttractorTick } from "./attractors"
 import { fArray } from "./utils"
 import { createPlayer } from "./createPlayer"
 import * as dat from "dat.gui"
@@ -19,20 +19,21 @@ gl.clearColor(1.0, 1.0, 1.0, 1.0)
 
 const positionData = fArray(100000, () => Math.random() * 6 - 3)
 const particles = createPSProgram(gl)
-const orbit = OrbitControls()
+const orbit = OrbitControls(canvas)
 
 
 
 const gui = new dat.GUI()
 const attractor = {
   attractor: "thomas",
-  list: [ "thomas", "aizawa", "halvorsen" ],
+  list: [ "thomas", "aizawa", "halvorsen", "lorenz", "threeScroll", "deJong" ],
   current: thomasAttractorTick,
+  roughness: 0.5,
 }
 
 gui
 .add(attractor, "attractor", attractor.list)
-.onChange((curr: "thomas" | "aizawa" | "halvorsen") => {
+.onChange((curr: string) => {
   if (curr === attractor.list[0]) {
     attractor.current = thomasAttractorTick
     orbit.a1 = -0.8
@@ -51,13 +52,34 @@ gui
     orbit.a2 = -0.7
     orbit.k = 80
   }
+  else if (curr === attractor.list[3]) {
+    attractor.current = lorenzAttractorTick
+    orbit.a1 = 0
+    orbit.a2 = 0
+    orbit.k = 20
+  }
+  else if (curr === attractor.list[4]) {
+    attractor.current = threeScrollAttractorTick
+    orbit.a1 = 0
+    orbit.a2 = 0
+    orbit.k = 3
+  }
+  else if (curr === attractor.list[5]) {
+    attractor.current = deJongAttractorTick
+    orbit.a1 = 0
+    orbit.a2 = 0
+    orbit.k = 100
+  }
 })
+
+gui
+.add(attractor, "roughness", 0, 1, 0.001)
 
 
 
 export const play = createPlayer((t: number) => {
   gl.clear(gl.COLOR_BUFFER_BIT)
 
-  attractor.current(positionData, t)
+  attractor.current(positionData, t, (attractor.roughness * attractor.roughness) * 0.05)
   particles(positionData, innerWidth, innerHeight, t, orbit)
 })
