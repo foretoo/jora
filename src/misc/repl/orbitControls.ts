db2982a7dc003820fec440bc63a1ac758ad266a7
Sergey Borovikov <serg.there@gmail.com>
2022-08-23 18:36:08 +0300

    listen to mouse on canvas only

--- src/misc/repl/orbitControls.ts
+++ src/misc/repl/orbitControls.ts
@@ -5,6 +5,7 @@ type PointData = { x: number, y: number, a1: number, a2: number }
 export type Orbit = { a1: number, a2: number, k: number }
 
 export function OrbitControls(
+  canvas: HTMLCanvasElement,
   a1 = -0.8,
   a2 = 1,
   k = 100,
@@ -12,12 +13,11 @@ export function OrbitControls(
 ) {
   const out = { a1, a2, k }
 
-  const listen = (type: MouseEventType, fn: (e: MouseEvent | WheelEvent) => void) => addEventListener(type, fn)
+  const listen = (type: MouseEventType, fn: (e: MouseEvent | WheelEvent) => void) => canvas.addEventListener(type, fn)
 
   listen("wheel", (e) => {
     const dir = Math.sign((e as WheelEvent).deltaY)
     out.k *= 1 - dir * 0.1
-    console.log(out)
   })
   
   listen("mouseup", () => {
@@ -37,7 +37,6 @@ export function OrbitControls(
     if (p) {
       out.a1 = p.a1 - (e.x - p.x) / 100
       out.a2 = p.a2 - (e.y - p.y) / 100
-      console.log(out)
     }
   })
 
