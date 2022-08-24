298261d677b163abdb9c4ffadb987864a6b5fb71
Sergey Borovikov <serg.there@gmail.com>
2022-08-24 23:47:31 +0300

    add *.vs, *.fs shader modules

--- src/glsl.d.ts
+++ src/glsl.d.ts
@@ -2,3 +2,13 @@ declare module "*.glsl" {
   const shader: string
   export default shader
 }
+
+declare module "*.vs" {
+  const shader: string
+  export default shader
+}
+
+declare module "*.fs" {
+  const shader: string
+  export default shader
+}
