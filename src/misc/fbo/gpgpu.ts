6d1abb8cfa33ba7d19cd4be8c72ec0ea588baf59
Sergey Borovikov <serg.there@gmail.com>
2022-08-25 23:17:09 +0300

    add gui, handle two attractors

--- src/misc/fbo/gpgpu.ts
+++ src/misc/fbo/gpgpu.ts
@@ -1,6 +1,69 @@
 import { RepeatWrapping, WebGLRenderer } from "three"
-import { GPUComputationRenderer } from "three/examples/jsm/misc/GPUComputationRenderer"
-import computeShader from "./shaders/compute.glsl"
+import { GPUComputationRenderer, Variable } from "three/examples/jsm/misc/GPUComputationRenderer"
+import thomasShader from "./shaders/thomas.glsl"
+import aizawaShader from "./shaders/aizawa.glsl"
+
+import * as dat from "dat.gui"
+
+let positionVar: Variable
+
+const gui = new dat.GUI()
+const attractor = {
+  attractor: "thomas",
+  list: [ "thomas", "aizawa" ],
+  
+  roughness: 0.0,
+  vel: 2.0,
+
+  a: 1.0,
+  b: 0.5,
+  c: 1.5,
+  d: 0.0,
+  e: 0.2,
+  skew: 0.0,
+}
+
+gui.add(attractor, "attractor", attractor.list)
+.onChange((curr: string) => {
+  if (curr === attractor.list[0]) {
+    positionVar.material.fragmentShader = thomasShader
+    positionVar.material.needsUpdate = true
+  }
+  else if (curr === attractor.list[1]) {
+    positionVar.material.fragmentShader = aizawaShader
+    positionVar.material.needsUpdate = true
+  }
+})
+
+gui.add(attractor, "roughness", 0, 1, 0.01)
+.onChange((roughness: number) => {
+  positionVar.material.uniforms.roughness.value = roughness
+})
+gui.add(attractor, "vel", 0, 4, 0.05)
+.onChange((vel: number) => {
+  positionVar.material.uniforms.vel.value = vel
+})
+
+gui.add(attractor, "a", -4, 4, 0.05)
+.onChange((value: number) => {
+  positionVar.material.uniforms.a.value = value
+})
+gui.add(attractor, "b", -4, 4, 0.05)
+.onChange((value: number) => {
+  positionVar.material.uniforms.b.value = value
+})
+gui.add(attractor, "c", -4, 4, 0.05)
+.onChange((value: number) => {
+  positionVar.material.uniforms.c.value = value
+})
+gui.add(attractor, "e", 0, 1, 0.05)
+.onChange((value: number) => {
+  positionVar.material.uniforms.e.value = value
+})
+gui.add(attractor, "skew", -0.5, 0.5, 0.01)
+.onChange((value: number) => {
+  positionVar.material.uniforms.f.value = value
+})
 
 export const getGPGPU = (
   width: number,
@@ -8,9 +71,8 @@ export const getGPGPU = (
   renderer: WebGLRenderer,
 ) => {
   const gpuCompute = new GPUComputationRenderer( width, height, renderer )
-  const positionTexture = gpuCompute.createTexture()
 
-  const data = positionTexture.image.data
+  const data = new Float32Array(width * height * 4)
   for (let i = 0; i < data.length; i += 4) {
     data[i + 0] = Math.random() * 2 - 1
     data[i + 1] = Math.random() * 2 - 1
@@ -18,20 +80,28 @@ export const getGPGPU = (
     data[i + 3] = 1
   }
 
-  const posVar = gpuCompute.addVariable("positionTexture", computeShader, positionTexture)
+  const positionTexture = gpuCompute.createTexture()
+  positionTexture.image.data.set(data)
+  positionVar = gpuCompute.addVariable("positionTexture", thomasShader, positionTexture)
 
-  posVar.material.uniforms.time =  { value: 0 }
-  posVar.material.uniforms.vel = { value: 0.03 }
-  posVar.material.uniforms.roughness = { value: 0.5 }
-  posVar.wrapS = RepeatWrapping
-  posVar.wrapT = RepeatWrapping
+  positionVar.material.uniforms.vel = { value: attractor.vel }
+  positionVar.material.uniforms.roughness = { value: attractor.roughness }
+
+  positionVar.material.uniforms.a = { value: attractor.a }
+  positionVar.material.uniforms.b = { value: attractor.b }
+  positionVar.material.uniforms.c = { value: attractor.c }
+  positionVar.material.uniforms.d = { value: attractor.d }
+  positionVar.material.uniforms.e = { value: attractor.e }
+  positionVar.material.uniforms.f = { value: attractor.skew }
+
+  positionVar.wrapS = RepeatWrapping
+  positionVar.wrapT = RepeatWrapping
 
   const error = gpuCompute.init()
   if ( error !== null ) console.error( error )
 
   return () => {
-    posVar.material.uniforms.time.value += 0.01
     gpuCompute.compute()
-    return gpuCompute.getCurrentRenderTarget(posVar).texture
+    return gpuCompute.getCurrentRenderTarget(positionVar).texture
   }
 }
