1e2353fac996373efa82ac7345d3f22507e2e5ef
Sergey Borovikov <serg.there@gmail.com>
2022-08-28 00:14:24 +0300

    add getPoints with try/catch to handle exeptions

--- src/classic/17-house/snowfellas.ts
+++ src/classic/17-house/snowfellas.ts
@@ -5,9 +5,10 @@ import PoissonDiskSampling from "poisson-disk-sampling"
 
 export const getSnowFellas = (
   radius: number,
-  count: number,
   noiseData: Uint8Array
 ) => {
+
+  const size = Math.sqrt(noiseData.length / 4)
 
   const getValue = (
     x: number,
@@ -20,29 +21,30 @@ export const getSnowFellas = (
     return noiseData[i] / 255
   }
 
-  const size = Math.sqrt(noiseData.length / 4)
-
-  const pds = new PoissonDiskSampling({
-    shape: [ size, size ],
-    minDistance: 6,
-    maxDistance: 66,
-    tries: 20,
-    distanceFunction: (p: [ number, number ]) => {
-      const v = getValue(p[0], p[1])
-      return v > 0.2 ? 0 : 1
-    },
-  })
-
-  let points: [number, number][] = pds.fill()
-  
-  const ifFilled = () => {
-    if (points.length > 66) return
-    pds.reset()
-    points = pds.fill()
-    ifFilled()
+  let tryNum = 20
+  const getPoints = (): [number, number][] | undefined => {
+    try {
+      tryNum--
+      const points: [number, number][] = new PoissonDiskSampling({
+        shape: [ size, size ],
+        minDistance: 6,
+        maxDistance: 66,
+        tries: 20,
+        distanceFunction: (p: [ number, number ]) => {
+          const v = getValue(p[0], p[1])
+          return v > 0.2 ? 0 : 1
+        },
+      }).fill()
+      if (points?.length < 50) throw undefined
+      return points
+    }
+    catch (err) {
+      return tryNum ? getPoints() : undefined
+    }
   }
-  ifFilled()
-  count = points.length
+
+  const points = getPoints()
+  const count = points?.length || 0
 
   const geometry = new SphereBufferGeometry(radius, 18, 6, 0, Math.PI * 2, 0, Math.PI / 2)
   const material = new MeshStandardMaterial({ color: "#fff", side: DoubleSide })
@@ -51,8 +53,8 @@ export const getSnowFellas = (
   const gizmo = new Object3D()
 
   for (let i = 0; i < count; i++) {
-    const px = points[i][0]
-    const py = points[i][1]
+    const px = points![i][0]
+    const py = points![i][1]
     const ox = px / size * 10 - 5
     const oy = 5 - py / size * 10
     gizmo.position.set(
