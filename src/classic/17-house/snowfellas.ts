88957473ef74217010e99bef6f83edb9c91fbb9e
Sergey Borovikov <serg.there@gmail.com>
2022-08-27 23:25:20 +0300

    add PoissonDiskSampling to distribute fellas

--- src/classic/17-house/snowfellas.ts
+++ src/classic/17-house/snowfellas.ts
@@ -1,10 +1,48 @@
 import { DoubleSide, InstancedMesh, MeshStandardMaterial, Object3D, SphereBufferGeometry } from "three"
-import { gRandom } from "../../utils"
+import PoissonDiskSampling from "poisson-disk-sampling"
+
+
 
 export const getSnowFellas = (
   radius: number,
-  count: number
+  count: number,
+  noiseData: Uint8Array
 ) => {
+
+  const getValue = (
+    x: number,
+    y: number,
+  ) => {
+    const
+    _x = Math.floor(x),
+    _y = Math.floor(y),
+    i = (_y * size + _x) * 4
+    return noiseData[i] / 255
+  }
+
+  const size = Math.sqrt(noiseData.length / 4)
+
+  const pds = new PoissonDiskSampling({
+    shape: [ size, size ],
+    minDistance: 6,
+    maxDistance: 66,
+    tries: 20,
+    distanceFunction: (p: [ number, number ]) => {
+      const v = getValue(p[0], p[1])
+      return v > 0.2 ? 0 : 1
+    },
+  })
+
+  let points: [number, number][] = pds.fill()
+  
+  const ifFilled = () => {
+    if (points.length > 66) return
+    pds.reset()
+    points = pds.fill()
+    ifFilled()
+  }
+  ifFilled()
+  count = points.length
 
   const geometry = new SphereBufferGeometry(radius, 18, 6, 0, Math.PI * 2, 0, Math.PI / 2)
   const material = new MeshStandardMaterial({ color: "#fff", side: DoubleSide })
@@ -13,16 +51,19 @@ export const getSnowFellas = (
   const gizmo = new Object3D()
 
   for (let i = 0; i < count; i++) {
-    const angle = gRandom(-Math.PI, Math.PI * 3)
-    const distance = 1.5 + (radius * 5) + Math.random() * (3.5 - (radius * 5))
+    const px = points[i][0]
+    const py = points[i][1]
+    const ox = px / size * 10 - 5
+    const oy = 5 - py / size * 10
     gizmo.position.set(
-      Math.sin(angle) * distance,
+      Math.abs(ox) > 4.5 ? 4.5 * Math.sign(ox) : ox,
       0,
-      Math.cos(angle) * distance,
+      Math.abs(oy) > 4.5 ? 4.5 * Math.sign(oy) : oy,
     )
-    const scale = 1 + Math.random() * 3
-    const scaleY = scale / 2 + Math.random() * scale * 5
-    gizmo.scale.set(scale, scaleY, scale)
+    const v = getValue(px, py)
+    const scaleH = 1 + v * Math.random() * 5
+    const scaleV = v * scaleH * 5
+    gizmo.scale.set(scaleH, scaleV, scaleH)
   
     gizmo.updateMatrix()
     mesh.setMatrixAt(i, gizmo.matrix)
