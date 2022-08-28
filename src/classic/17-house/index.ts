63f668654a3e7a3a1c53c7e8533b7480d349b572
Sergey Borovikov <serg.there@gmail.com>
2022-08-28 03:03:39 +0300

    add shadows

--- src/classic/17-house/index.ts
+++ src/classic/17-house/index.ts
@@ -1,7 +1,7 @@
 import { scene, camera, renderer } from "../../init"
 import * as dat from "dat.gui"
 import { igloo, indices } from "./igloo"
-import { AmbientLight, CapsuleBufferGeometry, DirectionalLight, DirectionalLightHelper, DoubleSide, GridHelper, Group, InstancedMesh, Mesh, MeshStandardMaterial, Object3D, PlaneBufferGeometry, Quaternion, Vector3 } from "three"
+import { AmbientLight, CameraHelper, CapsuleBufferGeometry, DirectionalLight, DirectionalLightHelper, DoubleSide, GridHelper, Group, InstancedMesh, Mesh, MeshStandardMaterial, Object3D, PCFSoftShadowMap, PlaneBufferGeometry, Quaternion, Vector2, Vector3 } from "three"
 import { getSnowFellas } from "./snowfellas"
 import { getRTTData } from "./rtt"
 
@@ -10,7 +10,13 @@ camera.position.set(2, 5, 8)
 
 const { texture: noiseTexture, buffer: noiseBuffer } = getRTTData(renderer)
 const fellas = getSnowFellas(0.1, noiseBuffer)
+fellas.material.flatShading = true
+fellas.castShadow = true
+fellas.receiveShadow = true
 scene.add(fellas)
+
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = PCFSoftShadowMap
 
 
 
@@ -30,6 +36,8 @@ const walls = new Mesh(
 )
 walls.material.side = DoubleSide
 walls.material.flatShading = true
+walls.castShadow = true
+walls.receiveShadow = true
 house.add(walls)
 
 
@@ -50,19 +58,21 @@ indices.forEach(([ x, y, z ], i) => {
   stickGismo.updateMatrix()
   sticks.setMatrixAt(i, stickGismo.matrix)
 })
+sticks.castShadow = true
 house.add(sticks)
 
 
 //// FLOOR
 
 const floor = new Mesh(
-  new PlaneBufferGeometry(10, 10),
+  new PlaneBufferGeometry(12, 12),
   new MeshStandardMaterial({
     color: "#adf",
     map: noiseTexture,
   }),
 )
 floor.rotation.x = -Math.PI / 2
+floor.receiveShadow = true
 scene.add(floor)
 
 
@@ -71,14 +81,30 @@ scene.add(floor)
  * LIGHTS
  */
 
-const ambientLight = new AmbientLight(0xffffff, 0.75)
+const ambientLight = new AmbientLight(0xffffff, 0.5)
 scene.add(ambientLight)
 
-const moonLight = new DirectionalLight("#f70", 0.75)
-moonLight.position.set(4, 5, -2)
+const moonLight = new DirectionalLight("#fa7", 0.9)
+moonLight.position.set(6, 4, -2)
+moonLight.castShadow = true
+moonLight.shadow.bias = 0.00002
+moonLight.shadow.mapSize = new Vector2(1024, 1024)
+moonLight.shadow.normalBias = 0.04
+
 const directHelper = new DirectionalLightHelper(moonLight, 0.2)
 setTimeout(() => directHelper.update(), 0)
-scene.add(moonLight, directHelper)
+
+const directCamera = new CameraHelper(moonLight.shadow.camera)
+moonLight.shadow.camera.near = 1
+moonLight.shadow.camera.far = 15
+moonLight.shadow.camera.left = -8
+moonLight.shadow.camera.right = 8
+moonLight.shadow.camera.top = 8
+moonLight.shadow.camera.bottom = -8
+moonLight.shadow.camera.updateProjectionMatrix()
+setTimeout(() => directCamera.update(), 0)
+// directCamera.visible = false
+scene.add(moonLight, directHelper, directCamera)
 
 
 
