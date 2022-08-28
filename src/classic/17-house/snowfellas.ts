d6a04f969e10e94c60b95977cce03c826f7066d5
Sergey Borovikov <serg.there@gmail.com>
2022-08-28 03:02:32 +0300

    tune geometry, set rotation

--- src/classic/17-house/snowfellas.ts
+++ src/classic/17-house/snowfellas.ts
@@ -27,8 +27,8 @@ export const getSnowFellas = (
       tryNum--
       const points: [number, number][] = new PoissonDiskSampling({
         shape: [ size, size ],
-        minDistance: 16,
-        maxDistance: 104,
+        minDistance: 18,
+        maxDistance: 108,
         tries: 40,
         distanceFunction: (p: [ number, number ]) => {
           const v = getValue(p[0], p[1])
@@ -46,7 +46,7 @@ export const getSnowFellas = (
   const points = getPoints()
   const count = points?.length || 0
 
-  const geometry = new SphereBufferGeometry(radius, 18, 6, 0, Math.PI * 2, 0, Math.PI / 2) // new ConeGeometry(radius, radius * 2, 12, 1, true) // new CylinderBufferGeometry(radius / 2, radius, radius * 2, 12, 1)
+  const geometry = new SphereBufferGeometry(radius, 4, 1, 0, Math.PI * 2, 0, Math.PI / 2) // new ConeGeometry(radius, radius * 2, 12, 1, true) // new CylinderBufferGeometry(radius / 2, radius, radius * 2, 12, 1)
   const material = new MeshStandardMaterial({ color: "#fff", side: DoubleSide })
   const mesh = new InstancedMesh(geometry, material, count)
 
@@ -58,12 +58,18 @@ export const getSnowFellas = (
     const py = points![i][1]
     const ox = px / size * 10 - 5
     const oy = 5 - py / size * 10
-    gizmo.position.set(ox, 0, oy)
+    gizmo.position.set(ox, -0.1, oy)
 
     const v = Math.max(getValue(px, py), 0.25)
     const scaleH = 1 + v * Math.random() * 9
     const scaleV = v * scaleH * 3
     gizmo.scale.set(scaleH, scaleV, scaleH)
+
+    gizmo.rotation.set(
+      Math.random() / 3,
+      Math.random() * Math.PI,
+      0
+    )
   
     gizmo.updateMatrix()
     mesh.setMatrixAt(i, gizmo.matrix)
