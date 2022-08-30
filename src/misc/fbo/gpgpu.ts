b5c8c1917bf1d3ded9e2ffda430310347a9b34c4
Sergey Borovikov <serg.there@gmail.com>
2022-08-30 17:19:48 +0300

    add listeners, get initial positions from sphere

--- src/misc/fbo/gpgpu.ts
+++ src/misc/fbo/gpgpu.ts
@@ -1,10 +1,9 @@
 import { RepeatWrapping, WebGLRenderer } from "three"
-import { GPUComputationRenderer, Variable } from "three/examples/jsm/misc/GPUComputationRenderer"
+import { GPUComputationRenderer } from "three/examples/jsm/misc/GPUComputationRenderer"
 import { controls } from "./controls"
+import { sphere } from "./sphere"
 
 
-
-export let positionVar: Variable
 
 export const getGPGPU = (
   width: number,
@@ -13,17 +12,21 @@ export const getGPGPU = (
 ) => {
   const gpuCompute = new GPUComputationRenderer(width, height, renderer)
 
+  const l = sphere.geometry.attributes.position.count * 3
+  const p = sphere.geometry.attributes.position.array
+
+
   const data = new Float32Array(width * height * 4)
-  for (let i = 0; i < data.length; i += 4) {
-    data[i + 0] = Math.random() * 2 - 1
-    data[i + 1] = Math.random() * 2 - 1
-    data[i + 2] = Math.random() * 2 - 1
-    data[i + 3] = 1
+  for (let i = 0; i < data.length; i++) {
+    data[i * 4 + 0] = p[(i * 3 + 0) % l]
+    data[i * 4 + 1] = p[(i * 3 + 1) % l]
+    data[i * 4 + 2] = p[(i * 3 + 2) % l]
+    data[i * 4 + 3] = 1
   }
 
   const positionTexture = gpuCompute.createTexture()
   positionTexture.image.data.set(data)
-  positionVar = gpuCompute.addVariable("positionTexture", controls.shader, positionTexture)
+  const positionVar = gpuCompute.addVariable("positionTexture", controls.shader, positionTexture)
 
   positionVar.material.uniforms.vel = { value: controls.vel }
   positionVar.material.uniforms.roughness = { value: controls.roughness }
@@ -35,6 +38,22 @@ export const getGPGPU = (
   positionVar.material.uniforms.ad = { value: controls.aizawa.d }
   positionVar.material.uniforms.ae = { value: controls.aizawa.e }
   positionVar.material.uniforms.af = { value: controls.aizawa.f }
+
+  controls.listen.roughness.push((v) => positionVar.material.uniforms.roughness.value = v)
+  controls.listen.vel.push((v) => positionVar.material.uniforms.vel.value = v)
+  
+  controls.listen.thomas.b.push((v) => positionVar.material.uniforms.tb.value = v)
+  controls.listen.aizawa.a.push((v) => positionVar.material.uniforms.aa.value = v)
+  controls.listen.aizawa.b.push((v) => positionVar.material.uniforms.ab.value = v)
+  controls.listen.aizawa.c.push((v) => positionVar.material.uniforms.ac.value = v)
+  controls.listen.aizawa.d.push((v) => positionVar.material.uniforms.ad.value = v)
+  controls.listen.aizawa.e.push((v) => positionVar.material.uniforms.ae.value = v)
+  controls.listen.aizawa.f.push((v) => positionVar.material.uniforms.af.value = v)
+
+  controls.listen.attractor.push((shader) => {
+    positionVar.material.fragmentShader = shader
+    positionVar.material.needsUpdate = true
+  })
 
   positionVar.wrapS = RepeatWrapping
   positionVar.wrapT = RepeatWrapping
