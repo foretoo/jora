bfa93d00628522b6349792fa11330073856dce92
Sergey Borovikov <serg.there@gmail.com>
2022-08-30 17:52:31 +0300

    add spheres

--- src/misc/fbo/index.ts
+++ src/misc/fbo/index.ts
@@ -1,21 +1,26 @@
 import { camera, orbit, renderer, scene } from "./init"
-
 import { initGUI } from "./gui"
-import { updateAttractor } from "./attractor"
+// import { updateAttractor } from "./attractor"
+import { initSphere } from "./sphere"
 
 
 
 initGUI()
 camera.position.set(0, 0, 12)
 
-
+const updateSphere1 = initSphere(Math.random() * 123, 0.2)
+const updateSphere2 = initSphere(Math.random() * 123, 0.25)
+const updateSphere3 = initSphere(Math.random() * 123, 0.3)
 
 
 
 let t = 0
 export const play = () => {
   t += 0.01
-  updateAttractor(t)
+  // updateAttractor(t)
+  updateSphere1(t)
+  updateSphere2(t/2)
+  updateSphere3(t/3)
 
   orbit.update()
   renderer.render(scene, camera)
