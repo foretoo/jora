63b3c6a9296f64721c131fdd61a5254d9a9600a4
Sergey Borovikov <serg.there@gmail.com>
2022-08-30 17:37:04 +0300

    isolate attractor

---
+++ src/misc/fbo/attractor.ts
@@ -0,0 +1,48 @@
+import { BufferAttribute, BufferGeometry, Points, ShaderMaterial } from "three"
+import { renderer, scene } from "./init"
+import { controls } from "./controls"
+import { getGPGPU } from "./gpgpu"
+
+import vertexShader from "./shaders/vertex.glsl"
+import fragmentShader from "./shaders/fragment.glsl"
+
+
+
+const width  = 256
+const height = 256
+const computePositionTexture = getGPGPU(width, height, renderer)
+
+
+export const material = new ShaderMaterial({
+  uniforms: {
+    time: { value: 0 },
+    noiseScale: { value: 1 / controls.noiseScale },
+    noiseStrength: { value: controls.noiseStrength },
+    positionTexture: { value: null },
+  },
+  vertexShader,
+  fragmentShader,
+  transparent: true,
+})
+controls.listen.noiseScale.push((v) => material.uniforms.noiseScale.value = v)
+controls.listen.noiseStrength.push((v) => material.uniforms.noiseStrength.value = v)
+
+
+const geometry = new BufferGeometry()
+const position = new Float32Array(width * height * 3)
+const reference = new Float32Array(width * height * 2)
+for (let i = 0; i < width * height; i++) {
+  reference[i * 2 + 0] = (i % width) / width
+  reference[i * 2 + 1] = (i / width | 0) / height
+}
+geometry.setAttribute("position", new BufferAttribute(position, 3))
+geometry.setAttribute("reference", new BufferAttribute(reference, 2))
+
+
+const points = new Points(geometry, material)
+scene.add(points)
+
+export const updateAttractor = (t: number) => {
+  material.uniforms.time.value = t
+  material.uniforms.positionTexture.value = computePositionTexture()
+}
--- src/misc/fbo/index.ts
+++ src/misc/fbo/index.ts
@@ -1,58 +1,22 @@
-import { BufferAttribute, BufferGeometry, Points, ShaderMaterial } from "three"
 import { camera, orbit, renderer, scene } from "./init"
-import { controls } from "./controls"
-import { getGPGPU } from "./gpgpu"
-import { initGUI } from "./gui"
 
-import vertexShader from "./shaders/vertex.glsl"
-import fragmentShader from "./shaders/fragment.glsl"
+import { initGUI } from "./gui"
+import { updateAttractor } from "./attractor"
 
 
 
 initGUI()
 camera.position.set(0, 0, 12)
 
-const width  = 256
-const height = 256
 
-const computePositionTexture = getGPGPU(width, height, renderer)
-
-
-
-export const material = new ShaderMaterial({
-  uniforms: {
-    time: { value: 0 },
-    noiseScale: { value: 1 / controls.noiseScale },
-    noiseStrength: { value: controls.noiseStrength },
-    positionTexture: { value: null },
-  },
-  vertexShader,
-  fragmentShader,
-  transparent: true,
-})
-controls.listen.noiseScale.push((v) => material.uniforms.noiseScale.value = v)
-controls.listen.noiseStrength.push((v) => material.uniforms.noiseStrength.value = v)
-
-const geometry = new BufferGeometry()
-const position = new Float32Array(width * height * 3)
-const reference = new Float32Array(width * height * 2)
-for (let i = 0; i < width * height; i++) {
-  reference[i * 2 + 0] = (i % width) / width
-  reference[i * 2 + 1] = (i / width | 0) / height
-}
-geometry.setAttribute("position", new BufferAttribute(position, 3))
-geometry.setAttribute("reference", new BufferAttribute(reference, 2))
-
-const points = new Points(geometry, material)
-scene.add(points)
 
 
 
 let t = 0
 export const play = () => {
   t += 0.01
-  material.uniforms.time.value = t
-  material.uniforms.positionTexture.value = computePositionTexture()
+  updateAttractor(t)
+
   orbit.update()
   renderer.render(scene, camera)
   requestAnimationFrame(play)
