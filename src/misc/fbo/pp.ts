284a2476412efe79950e1e042a3ff7dc272879a1
Sergey Borovikov <serg.there@gmail.com>
2022-08-30 17:24:54 +0300

    add post-process composer

---
+++ src/misc/fbo/pp.ts
@@ -0,0 +1,38 @@
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js"
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js"
+import { AfterimagePass } from "three/examples/jsm/postprocessing/AfterimagePass.js"
+import { camera, renderer, scene } from "./init"
+import { IUniform } from "three"
+
+const composer = new EffectComposer( renderer )
+composer.addPass( new RenderPass( scene, camera ) )
+
+const afterimagePass = new AfterimagePass()
+interface IUniforms { [uniform: string]: IUniform<number> }
+(afterimagePass.uniforms as IUniforms).damp.value = 0.618
+
+afterimagePass.shaderMaterial.fragmentShader = `
+uniform float damp;
+
+uniform sampler2D tOld;
+uniform sampler2D tNew;
+
+varying vec2 vUv;
+
+void main() {
+
+  vec4 texelOld = texture2D( tOld, vUv );
+  vec4 texelNew = texture2D( tNew, vUv );
+
+  texelOld *= damp; // * max( texelOld - 0.01, 0.0 );
+
+  gl_FragColor = max(texelNew, texelOld);
+
+}`
+
+afterimagePass.shaderMaterial.needsUpdate = true
+composer.addPass( afterimagePass )
+
+export const renderComposer = () => {
+  composer.render()
+}
