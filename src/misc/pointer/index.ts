47c4d90130a01c3950831172d4e8ae8eaa6f99b8
Sergey Borovikov <serg.there@gmail.com>
2022-09-07 13:10:34 +0300

    isolate sphere

--- src/misc/pointer/index.ts
+++ src/misc/pointer/index.ts
@@ -1,38 +1,11 @@
-import { BufferAttribute, BufferGeometry, Points, ShaderMaterial } from "three"
-import { AMOUNT, INITIAL_DATA, SIDE } from "./const"
 import { camera, orbit, renderer, scene } from "./setup"
-import { compute } from "./gpu"
-import vertexShader from "./vertex.glsl"
-import fragmentShader from "./fragment.glsl"
+import { initiateSphere } from "./sphere"
 
 
 
 camera.position.set(0, 0, 3)
 
-
-
-const sphereGeometry = new BufferGeometry()
-
-const ref = new Float32Array(AMOUNT * 2)
-for (let i = 0; i < AMOUNT; i++) {
-  ref[i * 2 + 0] = (i % SIDE) / SIDE
-  ref[i * 2 + 1] = (i / SIDE | 0) / SIDE
-}
-sphereGeometry.setAttribute("position", new BufferAttribute(INITIAL_DATA, 4))
-sphereGeometry.setAttribute("ref", new BufferAttribute(ref, 2))
-
-
-
-const sphereMaterial = new ShaderMaterial({
-  uniforms: {
-    positionTexture: { value: null }
-  },
-  vertexShader,
-  fragmentShader,
-})
-
-const sphere = new Points(sphereGeometry, sphereMaterial)
-scene.add(sphere)
+const updateGPUSphere = initiateSphere()
 
 
 
@@ -40,8 +13,7 @@ let t = 0
 export const play = () => {
   t += 0.01
 
-  sphereMaterial.uniforms.positionTexture.value = compute(t)
-
+  updateGPUSphere(t)
   orbit.update()
   renderer.render(scene, camera)
   requestAnimationFrame(play)
---
+++ src/misc/pointer/sphere.ts
@@ -0,0 +1,41 @@
+import { BufferAttribute, BufferGeometry, Points, ShaderMaterial } from "three"
+import { AMOUNT, INITIAL_DATA, SIDE } from "./const"
+import { scene } from "./setup"
+import { compute } from "./gpu"
+import vertexShader from "./vertex.glsl"
+import fragmentShader from "./fragment.glsl"
+
+
+
+const sphereGeometry = new BufferGeometry()
+
+const ref = new Float32Array(AMOUNT * 2)
+for (let i = 0; i < AMOUNT; i++) {
+  ref[i * 2 + 0] = (i % SIDE) / SIDE
+  ref[i * 2 + 1] = (i / SIDE | 0) / SIDE
+}
+sphereGeometry.setAttribute("position", new BufferAttribute(INITIAL_DATA, 4))
+sphereGeometry.setAttribute("ref", new BufferAttribute(ref, 2))
+
+
+
+const sphereMaterial = new ShaderMaterial({
+  uniforms: {
+    positionTexture: { value: null }
+  },
+  vertexShader,
+  fragmentShader,
+})
+
+
+
+export const initiateSphere = () => {
+  const sphere = new Points(sphereGeometry, sphereMaterial)
+  scene.add(sphere)
+
+  return (
+    t: number
+  ) => {
+    sphereMaterial.uniforms.positionTexture.value = compute(t)
+  }
+}
