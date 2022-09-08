0af189369d4ad9e9dd2b942e2edbc165a8165d3f
Sergey Borovikov <serg.there@gmail.com>
2022-09-08 15:38:36 +0300

    get points from IcosahedronBufferGeometry

--- src/misc/pointer/icosah.ts
+++ src/misc/pointer/icosah.ts
@@ -1,18 +1,41 @@
-const phi = (1 + Math.sqrt(5)) / 2
+import { IcosahedronBufferGeometry } from "three"
 
-export const icosahpoints = [
-  -1 * phi,       -1,        0,
-  -1 * phi,        1,        0,
-       phi,       -1,        0,
-       phi,        1,        0,
-         0, -1 * phi,       -1,
-         0, -1 * phi,        1,
-         0,  1 * phi,       -1,
-         0,  1 * phi,        1,
-        -1,        0, -1 * phi,
-         1,        0, -1 * phi,
-        -1,        0,  1 * phi,
-         1,        0,  1 * phi,
-]
+// const phi = (1 + Math.sqrt(5)) / 2
 
-export const icosah = new Float32Array(icosahpoints)
+// export const icosahpoints = [
+//   -1 * phi,       -1,        0,
+//   -1 * phi,        1,        0,
+//        phi,       -1,        0,
+//        phi,        1,        0,
+//          0, -1 * phi,       -1,
+//          0, -1 * phi,        1,
+//          0,  1 * phi,       -1,
+//          0,  1 * phi,        1,
+//         -1,        0, -1 * phi,
+//          1,        0, -1 * phi,
+//         -1,        0,  1 * phi,
+//          1,        0,  1 * phi,
+// ]
+
+
+
+const ico = new IcosahedronBufferGeometry(1, 2)
+
+const icoP = ico.attributes.position.array
+const icoN = ico.attributes.position.count
+const icoS = ico.attributes.position.itemSize
+const icoA = icoN * icoS
+
+const icoarr: number[] = []
+const icoSet = new Set<string>()
+
+for (let i = 0; i < icoA; i += icoS) {
+  const p = [ icoP[i], icoP[i+1], icoP[i+2] ]
+  const pstr = JSON.stringify(p)
+  if (!icoSet.has(pstr)) {
+    icoarr.push(...p)
+    icoSet.add(pstr)
+  }
+}
+
+export const icosahpoints = new Float32Array(icoarr)
