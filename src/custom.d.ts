461dd5544c8d015e3bca7cc113312aef4a26cd3e
Sergey Borovikov <serg.there@gmail.com>
2023-03-13 14:53:15 +0000

    rn glsl to custom, add Tuple gen

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -12,3 +12,6 @@ declare module "*.fs" {
   const shader: string
   export default shader
 }
+
+type _TupleOf<T, N extends number, R extends unknown[]> = R['length'] extends N ? R : _TupleOf<T, N, [T, ...R]>;
+type Tuple<T, N extends number> = N extends N ? (number extends N ? T[] : _TupleOf<T, N, []>) : never;
